66b9c199503195b2d7bca7d5864dac38880c31f1
Necrotex <necrotex@gmail.com>
2011-04-11 11:30:31 +0200

    New Class dioxid\lib\Session. 	Easy session handling with namespaces

--- lib/Session.php
+++ lib/Session.php
@@ -8,14 +8,42 @@
  */
 
 namespace dioxid\lib;
+use dioxid\error\exception\NamespaceAllreadyExistsException;
+
 use dioxid\lib\base;
 
-class Session extends Base {
+class Session {
 
-	public static function init(){
+	protected $namespace;
+
+	public function __construct($namespace){
+		$this->namespace = $namespace;
 		session_start();
+		if(in_array($_SESSION, $namespace)) throw new NamespaceAllreadyExistsException("$namespace allready used in session");
+	}
+
+	public function __set($key, $val) {
+		if($key == 'id') {
+			session_id($val);
+			return true;
+		}
+
+		$_SESSION[$this->namespace][$key] = $val;
+		return true;
+	}
+
+	public function __get($key){
+		if($key == "id") return session_id();
+
+		if(in_array($_SESSION[$this->namespace], $key)){
+			return $_SESSION[$this->namespace][$key];
+		return false;
+		}
+	}
+
+	public function destroy(){
+		session_destroy();
 	}
 
 }
-
 ?>
