0a8a08e1fa8597c6b5c682cd607ab3a7487857d4
Necrotex <necrotex@gmail.com>
2011-04-29 15:47:00 +0200

    + Added Exceptionhandling to Configload methods + Added functionality to dynamicly add persistent and nonpersistent 	section, key and values to the config file 	+ new method: Config::setVal($section, $key, $value, $override) 	+ new method: Config::setValPermanent($section, $key, $value, 															$override)

--- config/Config.php
+++ config/Config.php
@@ -9,13 +9,17 @@
 
 namespace dioxid\config;
 
+use Exception;
+use dioxid\error\exception\RequiredValNotFoundException;
+use dioxid\error\exception\ConfigValueAlreadyExistsException;
+use dioxid\error\exception\CantAccessConfigFileException;
+
 /**
  * Interface to work globally with the Configfile
  * @author Andre 'Necrotex' Peiffer <necrotex@gmail.com>
  * @date 22.03.2011 14:16:21
  *
  */
-use dioxid\error\exception\RequiredValNotFoundException;
 
 class Config {
 
@@ -24,8 +28,7 @@ class Config {
 	 * @var array
 	 */
 	private static $config;
-
-	public function __construct() { }
+	private static $path;
 
 	/**
 	 * Loads a configfile
@@ -33,6 +36,7 @@ class Config {
 	 * @param bool $cahced should the config file get cached?
 	 */
 	public static function loadConfig($path, $cached=false){
+		static::$path = $path;
 		$cached ? static::loadCached($path) : static::loadIni($path);
 	}
 
@@ -49,7 +53,13 @@ class Config {
 
 		$cachefile = $folder . ".". $conf . ".cache";
 		if(file_exists($cachefile) && filectime($cachefile) > filectime($path)){
-			static::$config = unserialize(file_get_contents($cachefile));
+			try{
+				static::$config = unserialize(file_get_contents($cachefile));
+			}
+			catch (Exception $e) {
+				throw new CantAccessConfigFileException($e->getMessage());
+			}
+
 		} else {
 			$conf = static::loadIni($path);
 			file_put_contents($cachefile, serialize(static::$config));
@@ -62,10 +72,14 @@ class Config {
 	 * @param string $path path to the config file
 	 */
 	public static function loadIni($path){
-		//TODO: Try/catch that fucker if it fails
-		$time = microtime(true);
-		static::$config =  parse_ini_string(file_get_contents($path), true);
+		try{
+			static::$config =  parse_ini_string(file_get_contents($path), true);
+		}
+		catch (Exception $e){
+			throw new CantAccessConfigFileException();
+		}
 	}
+
 
 	/**
 	 * Method: getVal
@@ -95,8 +109,50 @@ class Config {
 		}
 	}
 
-	//TODO: add new values permanent or temp
 
+	public static function setVal($section, $key, $value, $override=false){
+		if(!$override && @key_exists($key, static::$config[$section]))
+			throw new ConfigValueAlreadyExistsException();
+
+		static::$config[$section][$key] = $value;
+	}
+
+	public static function setValPermanent($section, $key, $value, $override=false) {
+		static::setVal($section, $key, $value, $override);
+		static::write_config(static::$config);
+	}
+
+
+	//taken and modified from: http://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
+	protected static function write_config($assoc_arr){
+	    $content = "";
+
+        foreach ($assoc_arr as $key=>$elem) {
+			$content .= "[".$key."]\n";
+
+			foreach ($elem as $key2=>$elem2) {
+				$content .= "    ";
+            	if(is_array($elem2)) {
+					for($i=0;$i<count($elem2);$i++) {
+	                	$content .= $key2."[] = \"".$elem2[$i]."\"\n";
+    	            }
+				}
+
+				else if($elem2=="") $content .= $key2." = \n";
+                else $content .= $key2." = \"".$elem2."\"\n";
+			}
+			$content .= "\n";
+        }
+
+        try {
+        	$handle = fopen(static::$path, 'w');
+        	fwrite($handle, $content);
+        	fclose($handle);
+        }
+        catch (Exception $e) {
+			throw new CantAccessConfigFileException();
+        }
+	}
 }
 
 ?>
