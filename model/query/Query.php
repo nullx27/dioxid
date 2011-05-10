3471143d7d3c586feead7725b364cc13026b4953
Necrotex <necrotex@gmail.com>
2011-05-10 19:55:48 +0200

    progress commit

--- model/query/Query.php
+++ model/query/Query.php
@@ -13,211 +13,216 @@ use Exception;
 class Query {
 
 	private $_tname;
-	private $_type = false;
 
 	private $_adapter = false;
 
-	private $_what = '*';
-	private $_where = '';
+	const DISTINCT = 'distinct';
+    const COLUMNS = 'columns';
+    const FROM = 'from';
+    const INTO = 'into';
+    const UNION = 'union';
+    const WHERE = 'where';
+    const GROUP = 'group';
+    const HAVING = 'having';
+    const ORDER = 'order';
+    const USING = 'using';
+    const LIMIT_COUNT = 'limitcount';
+    const LIMIT_OFFSET = 'limitoffset';
 
-	private $_joinstack = array();
 
-	private $_modifier = '';
-	private $_aggregatorstack = array();
-	private $_limit = array();
+    const SET = 'set';
+    const VALUES = 'values';
+
+
+    const TYPE = 'type';
+    const SELECT = 'select';
+    const UPDATE = 'update';
+    const INSERT = 'insert';
+    const DELETE = 'delete';
+
+
+	const SQL_SELECT = 'SELECT';
+	const SQL_INSERT = 'INSERT INTO';
+	const SQL_UPDATE = 'UPDATE';
+	const SQL_DELETE = 'DELETE';
+	const SQL_UNION = 'UNION';
+
+	const SQL_DISINCT = 'DISINCT';
+
+	const SQL_FROM = 'FROM';
+
+	const SQL_INNER_JOIN = 'INNER JOIN';
+	const SQL_LEFT_JOIN = 'LEFT JOIN';
+	const SQL_RIGHT_JOIN = 'RIGHT JOIN';
+	const SQL_FULL_JOIN = 'FULL JOIN';
+	const SQL_CROSS_JOIN = 'CROSS JOIN';
+	const SQL_NATRUAL_JOIN = 'NATURAL JOIN';
+
+	const SQL_ON = 'ON';
+
+	const SQL_USING = 'USING';
+
+	const SQL_WHERE = 'WHERE';
+	const SQL_AND = 'AND';
+	const SQL_OR = 'OR';
+
+
+	const SQL_GROUP = 'GROUP BY';
+	const SQL_HAVING = 'HAVING';
+	const SQL_ORDER = 'ORDER BY';
+
+	const SQL_LIMIT = 'LIMIT';
+
+	const SQL_ASC = 'ASC';
+	const SQL_DESC = 'DESC';
+	const SQL_AS = 'AS';
+	const SQL_SET = 'SET';
+	const SQL_VALUES = 'VALUES';
+
+
+
+	private $_querystack = false;
+
+	private $_querystack_select_init = array(
+			self::TYPE => self::SELECT,
+			self::DISTINCT => false,
+			self::COLUMNS => array(),
+			self::UNION => array(),
+			self::FROM => array(),
+			self::WHERE => array(),
+			self::GROUP => array(),
+			self::HAVING => array(),
+			self::ORDER => array(),
+			self::LIMIT_COUNT => null,
+			self::LIMIT_OFFSET => null
+		);
+
+	private $_querystack_update_init = array(
+			self::TYPE => self::UPDATE,
+			self::COLUMNS => array(),
+			self::SET => array(),
+			self::WHERE => array(),
+		);
+
+	private $_querystack_insert_init = array(
+			self::TYPE => self::INSERT,
+			self::INTO => array(),
+			self::COLUMNS => array(),
+			self::VALUES => array()
+		);
+
+	private $_querystack_delete_init = array(
+			self::TYPE => self::DELETE',
+			self::FROM => array(),
+			self::WHERE => array(),
+			self::USING => array()
+		);
+
+
+	private $_WHERE_allowed = array(
+			self::SELECT,
+			self::DELETE,
+			self::UPDATE
+	);
+
+
 
 
 	public function __construct($tablename, $adapter){
 		$this->_tname = $tablename;
-
 		$this->_adapter = $adapter;
 	}
 
-	public function select ($what){
-		if($this->_type != "")
-			throw new Exception('Wrong Type, and replace me with my own exception please!');
-		$this->_type = 'SELECT';
-		if(!is_array($what))
-		{
-			$this->_what = $what;
-		} else {
-			$this->_what = implode(', ', $what);
-		}
-		return $this;
+
+
+	private function _unpack($array){
+
 	}
 
-	public function where($stmt){
-		if($this->_where != '')
-			$this->_where .= ' AND ';
+	/**
+	 * Method: select
+	 *
+	 * @param string|array $what
+	 */
+	public function select($what){
+		if($this->_querystack)
+			throw new Exception();
+		$this->_querystack = $this->_querystack_select_init;
+		$this->_querystack[self::FROM] = $this->_tname;
+	}
 
-		$this->_where .= trim($stmt);
-		return $this;
+	public function update($what) {
+		if($this->_querystack)
+			throw new Exception();
+
+		$this->_querystack = $this->_querystack_update_init;
+	}
+
+	public function insert($what){
+		if($this->_querystack)
+			throw new Exception();
+
+		$this->_querystack[self::INTO] = $this->_tname;
+	}
+
+	public function delete($what){
+		if($this->_querystack)
+			throw new Exception();
+
+		$this->_querystack[self::FROM] = $this->_tname;
+	}
+
+	public function where($where){
+		if(!in_array($this->_querystack[self::TYPE], $this->_WHERE_allowed))
+			throw new Exception();
+	}
+
+	public function orWhere($where){
+		if(!in_array($this->_querystack[self::TYPE], $this->_WHERE_allowed))
+			throw new Exception();
 	}
 
 
-	public function orWhere($stmt){
-		if($this->_where == '')
-			throw new Exception('No where statement where set before, also i need a own exception!');
+	public function from($where){
 
-		$this->_where .= ' OR ' . trim($stmt);
-		return $this;
 	}
 
-	public function join($what, $on){
-		$this->_joinstack['JOIN'][] = array($what => $on);
-		return $this;
+	/**
+	 * Method: join
+	 * assoc array: array("what" => "on")
+	 * ex: array("foo as f" => "f.b = bar.foo")
+	 *
+	 * @param array $join
+	 */
+	public function join(array $join){
+
 	}
 
-	public function rightjoin($what, $on){
-		$this->_joinstack['RIGHTJOIN'][] = array($what => $on);
-		return $this;
+	public function leftjoin(array $join){
+
 	}
 
-	public function outerjoin($what, $on){
-		$this->_joinstack['OUTERJOIN'][] = array($what => $on);
-		return $this;
+	public function rightjoin(array $join){
+
 	}
 
-	public function crossjoin($what, $on){
-		$this->_joinstack['CROSSJOIN'][] = array($what => $on);
-		return $this;
+	public function crossjoin(array $join){
+
+	}
+
+	public function fulljoin(array $join){
+
+	}
+
+	public function fulljoin(array $join){
+
 	}
 
 	public function distinct(){
-		if($this->_modifier != '')
+		if($this->_querystack[self::TYPE] != self::SELECT)
 			throw new Exception();
-		$this->_modifier = 'DISTINCT';
-		return $this;
 	}
 
-	public function limit($start, $end) {
-		$this->_limit = array($start, $end);
-		return $this;
-	}
-
-	public function groupby(array $what){
-		$this->_aggregatorstack[] = 'GROUP BY ' . implode(', ', $what);
-		return $this;
-	}
-
-	public function having($what){
-		$this->_aggregatorstack[] = 'HAVING (' . trim($what) . ')';
-		return $this;
-	}
-
-	public function orderby(array $what, $how = 'ASC'){
-		$this->_aggregatorstack[] = 'ORDER BY ' . implode(', ', $what) . " " . strtoupper(trim($how));
-		return $this;
-	}
-
-	public function insert(array $what){
-		if($this->_type != "")
-			throw new Exception('Wrong Type, and replace me with my own exception please!');
-		$this->_type = 'INSERT';
-
-		$cols = '';
-		$vals = '';
-		foreach($what as $col => $val){
-			$cols .= $col .', ';
-			$vals .= "'" . $val . "'" .', ';
-		}
-		$this->_what = "(" . trim($cols,', ') . ") VALUES (". trim($vals, ', ') . ")";
-		return $this;
-	}
-
-	public function update(array $what){
-		if($this->_type != "")
-			throw new Exception('Wrong Type, and replace me with my own exception please!');
-		$this->_type = 'UPDATE';
-		$out = '';
-
-		foreach($what as $col => $val){
-			$out .= "$col=$val, ";
-		}
-		$this->_what = trim($out, ', ');
-		return $this;
-	}
-
-	public function delete(){
-		if($this->_type != "")
-			throw new Exception('Wrong Type, and replace me with my own exception please!');
-		$this->_type = 'DELETE';
-		return $this;
-	}
-
-	private function _assemble(){
-		$query = '';
-		switch($this->_type){
-			case 'SELECT':
-				$query .= $this->_type;
-				if($this->_modifier != '') $query .= " " . $this->_modifier;
-				$query .= " " . $this->_what;
-				$query .= " FROM `" . $this->_tname . '`';
-
-				if(count($this->_joinstack) > 0) {
-					foreach ($this->_joinstack as $type => $counter){
-						foreach ($counter as $c => $con){
-							foreach($con as $what => $where) {
-								$query .= " ";
-								$query .= $type;
-								$query .= " " . $what ;
-								$query .= " ON " .$where;
-							}
-						}
-					}
-				}
-
-				if($this->_where != ''){
-					$query .= " WHERE " . $this->_where;
-				}
-
-				if(count($this->_aggregatorstack) > 0) {
-					foreach ($this->_aggregatorstack as $agg) {
-					$query .= " ";
-					$query .= $agg;
-				}
-
-				if(count($this->_limit) > 0){
-					$query .= " ";
-					$query .= "LIMIT " . $this->_limit[0] . ", " . $this->_limit[1];
-				}
-		}
-			break;
-		case 'INSERT':
-			$query .= 'INSERT INTO `' . $this->_tname . '`';
-			$query .= ' ' . $this->_what;
-			break;
-		case 'UPDATE':
-			$query .= "UPDATE";
-			$query .= ' `' . $this->_tname . '`';
-			$query .= ' SET ' . $this->_what;
-			$query .= ' WHERE ' . $this->_where;
-			break;
-		case 'DELETE':
-			$query .= "DELETE";
-			$query .= ' FROM `' . $this->_tname . '`';
-			$query .= ' WHERE ' . $this->_where;
-			break;
-	}
-
-	return trim($query);
-	}
-
-	public function _toString(){
-		return $this->_assemble();
-	}
-
-	public function exec(){
-		$query = $this->_assemble();
-		$this->_adapter->exec($query);
-		return $this->_adapter->lastInsertId();
-	}
-
-	public function fetch(){
-		$query = $this->_assemble();
-		$res = $this->_adapter->query($query);
-		return $res->fetchObject();
-	}
 }
 
-?>
+?> 8
